498163f1bf40cbddf6a6505d6ec8afef61561271
fanuelhpa <fanuelrique@gmail.com>
2026-08-24 23:11:01 -0300

    POST new product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -23,10 +23,29 @@ public class ProductService {
         return new ProductDTO(product);
     }
 
+    @Transactional(readOnly = true)
     public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> result = repository.findAll(pageable);
 
         //converte o page de Product para um page de ProductDTO
         return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+
+        //copia os dados do dto para o product
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        //salva no banco e retorna o objeto salvo (product salvo)
+        entity = repository.save(entity);
+
+        //retorna o dto passando a entidade para ser convertida em dto
+        return new ProductDTO(entity);
+
     }
 }
